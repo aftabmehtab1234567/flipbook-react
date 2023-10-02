08400f43bab84ec7282265235549d71426229392
aftab alam <aftabalam9008@gmail.com>
2023-10-02 00:51:46 -0400

    hi

--- src/component/FlipBook.jsx
+++ src/component/FlipBook.jsx
@@ -1,9 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
-import Flipbook from "react-pageflip";
+import Flipbook from 'react-pageflip';
 
-function FlipBook({  imageArray }) {
- console.log(imageArray);
-
+function FlipBook({ imageArray }) {
   const [currentPage, setCurrentPage] = useState(0);
   const flipPageRef = useRef(null);
 
@@ -13,33 +11,30 @@ function FlipBook({  imageArray }) {
   }, []);
 
   if (!Array.isArray(imageArray) || imageArray.length === 0) {
-   
     return <div>No images to display.</div>;
   }
 
   const handlePageChange = (newPage) => {
     setCurrentPage(newPage);
   };
-  
-  return (
-    
-    <div className="flipbook-container bg-secondary">
-      <Flipbook
-        ref={flipPageRef}
-        width={665} // Adjust the width as needed
-        height={830} // Adjust the height as needed
-        currentPage={currentPage}
-        onFlip={handlePageChange}
-      >
-        {imageArray.map((imageDataUrl, index) => (
-          <div key={index} className="page">
-            <img src={imageDataUrl} alt={`Page ${index + 1}`} />
-          </div>
-        ))}
-        
-      </Flipbook>
 
-      
+  return (
+    <div className="background-container">
+      <div className="centered-container">
+        <Flipbook
+          ref={flipPageRef}
+          width={580} // Adjust the width as needed
+          height={800} // Adjust the height as needed
+          currentPage={currentPage}
+          onFlip={handlePageChange}
+        >
+          {imageArray.map((imageDataUrl, index) => (
+            <div key={index} className="page">
+              <img src={imageDataUrl} alt={`Page ${index + 1}`} />
+            </div>
+          ))}
+        </Flipbook>
+      </div>
     </div>
   );
 }
